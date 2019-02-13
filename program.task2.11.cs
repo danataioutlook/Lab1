75e46a7b25f4c4d36a05053108a63dc639a83f04
danataioutlook <47568383+danataioutlook@users.noreply.github.com>
2019-02-13 21:52:23 +0600

    Add files via upload

---
+++ program.task2.11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int i, j; // rows
+            for( i = 1; i <= 3; i++)  //This is loop for arrange the space
+            {
+                for(j=1; j<=i;j++)  //This loop for arange the space
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
